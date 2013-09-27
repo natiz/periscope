1e224557b9261248e297edde896a4808708118d0
natiziv <natiiziv@gmail.com>
2013-09-27 18:25:00 +0200

    Update __init__.py

--- periscope/plugins/__init__.py
+++ periscope/plugins/__init__.py
@@ -17,7 +17,7 @@
 #    along with emesene; if not, write to the Free Software
 #    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-from SubCenter.org import SubCenter
+from SubsCenter.org import SubsCenter
 from OpenSubtitles import OpenSubtitles
 from Subtitulos import Subtitulos
 from Podnapisi import Podnapisi
